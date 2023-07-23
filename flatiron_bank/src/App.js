c8d78ce413821965e067ebabdf704b46e78f2684
bettayub <bettayub19@gmail.com>
2023-07-23 21:51:31 +0300

    Created the main layout of the App with components for search, transaction form, and transaction table

--- flatiron_bank/src/App.js
+++ flatiron_bank/src/App.js
@@ -24,4 +24,37 @@ const App = () => {
     setSearchTerm(term);
   };
 
-  
+
+  const addTransaction = (newTransaction) => {
+    const newId = transactions.length + 1;
+    const updatedTransactions = [...transactions, { id: newId, ...newTransaction }];
+    setTransactions(updatedTransactions);
+  };
+
+  const deleteTransaction = (id) => {
+    const updatedTransactions = transactions.filter((transaction) => transaction.id !== id);
+    setTransactions(updatedTransactions);
+  };
+
+  const filteredTransactions = transactions.filter(
+    (transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <h1>Bank of Flatiron</h1>
+      <div className="search-container">
+        <SearchBar handleSearch={handleSearch} />
+      </div>
+      <div className="form-container">
+        <TransactionForm addTransaction={addTransaction} />
+      </div>
+      <TransactionTable transactions={filteredTransactions} onDeleteTransaction={deleteTransaction} />
+    </div>
+  );
+};
+
+
+
+
