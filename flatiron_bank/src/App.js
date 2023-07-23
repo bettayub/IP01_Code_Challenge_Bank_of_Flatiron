f10e9b4825241dce893e37a3e20f565c72f693ea
bettayub <bettayub19@gmail.com>
2023-07-23 21:48:45 +0300

    Initialized App component with state for transactions and search term, and implemented API call to fetch transactions on component mount

--- flatiron_bank/src/App.js
+++ flatiron_bank/src/App.js
@@ -4,3 +4,24 @@ import SearchBar from './components/SearchBar';
 import TransactionForm from './components/TransactionForm';
 import TransactionTable from './components/TransactionTable';
 import axios from 'axios';
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch transactions from JSON server using axios
+    axios.get('http://localhost:3000/transactions')
+      .then((response) => {
+        setTransactions(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  
