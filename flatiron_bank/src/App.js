bc638be6b5cf685a99532bda335554c8942e13fd
bettayub <bettayub19@gmail.com>
2023-07-23 21:24:58 +0300

    Integrated axios for API calls and added components SearchBar, TransactionForm, and TransactionTable to App

--- flatiron_bank/src/App.js
+++ flatiron_bank/src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import SearchBar from './components/SearchBar';
+import TransactionForm from './components/TransactionForm';
+import TransactionTable from './components/TransactionTable';
+import axios from 'axios';
