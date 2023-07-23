a2773fb6bac4d78c443b10324ece1523c64288e3
bettayub <bettayub19@gmail.com>
2023-07-23 20:34:19 +0300

    Implemented a form for adding transactions with date, description, category, and amount fields

--- flatiron_bank/src/components/Transaction.js
+++ flatiron_bank/src/components/Transaction.js
@@ -19,3 +19,31 @@ const TransactionForm = ({ addTransaction }) => {
     setFormData({ date: '', description: '', category: '', amount: '' });
   };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Date:
+        <input type="date" name="date" value={formData.date} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Description:
+        <input type="text" name="description" value={formData.description} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Category:
+        <input type="text" name="category" value={formData.category} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Amount:
+        <input type="number" name="amount" value={formData.amount} onChange={handleChange} />
+      </label>
+      <br />
+      <button type="submit">Add Transaction</button>
+    </form>
+  );
+};
+
+
